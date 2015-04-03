557a967c93459b9c92a36a788de37272c3ceec87
Ed Gamble <ebg@opuslogica.com>
2015-04-03 15:29:34 -0700

    Cleaned

--- src/ios/OLIRecorder.m
+++ src/ios/OLIRecorder.m
@@ -19,8 +19,8 @@ static AudioStreamingRecorder *theAudioRecorder = nil;
 }
 
 - (CDVPlugin*) initWithWebView: (UIWebView*) theWebView {
-  [super initWithWebView: theWebView];
-  
+  self = [super initWithWebView: theWebView];
+  return self;
 }
 
 //
@@ -38,7 +38,8 @@ static AudioStreamingRecorder *theAudioRecorder = nil;
 
   if (nil != self.audioRecorder && nil != self.audioRecorder.queue) {
     self.audioMeter = [[AQLevelMeter alloc] initWithFrame:
-                       CGRectMake(posX, posY, width, height)];
+                       CGRectMake(posX.integerValue, posY.integerValue,
+                                  width.integerValue, height.integerValue)];
     [self.audioMeter setBorderColor: [UIColor lightGrayColor]];
     [self.audioMeter setBackgroundColor: [UIColor yellowColor]];
   
@@ -313,3 +314,73 @@ static AudioStreamingRecorder *theAudioRecorder = nil;
 }
 
 @end
+
+//
+//
+//
+#if 0
+#import "AudioStreamingRecorder.h"
+#import "AQLevelMeter.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UIButton *buttonToggleRecord;
+
+@property AudioStreamingRecorder *audioRecorder;
+@property AQLevelMeter *audioMeter;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+- (void) viewWillAppear:(BOOL)animated {
+  [super viewWillAppear: animated];
+  self.webView.scalesPageToFit = false;
+  [self.webView loadRequest:
+   [NSURLRequest requestWithURL:
+    [NSURL URLWithString: @"https://imgur.com/gallery/7bp3Zd2"]]];
+}
+
+- (IBAction)toggleRecord:(id)sender {
+  if (nil == self.audioRecorder) {
+    self.audioRecorder = [[AudioStreamingRecorder alloc] initWithRecordingInterval: 10.0];
+    [self.audioRecorder configureWithCallback: ^(unsigned int session, unsigned int block, NSURL *file) {
+      NSLog (@"Callback: %d, %d, %@", session, block, file);
+    }];
+  }
+  
+  if (self.audioRecorder.isRecording) {
+    [self.audioRecorder pause];
+    self.audioMeter.aq = nil;
+    //self.audioMeter.hidden = YES;
+    [self.audioMeter removeFromSuperview];
+    self.audioMeter = nil;
+  }
+  else {
+    [self.audioRecorder record];
+    // self.audioMeter.hidden = NO;
+    self.audioMeter = [[AQLevelMeter alloc] initWithFrame:
+                       CGRectMake(20, 200, 320, 50)];
+    [self.audioMeter setBorderColor: [UIColor whiteColor]];
+    [self.audioMeter setBackgroundColor: [UIColor yellowColor]];
+    
+    self.audioMeter.aq = self.audioRecorder.queue;
+    
+    [self.view addSubview: self.audioMeter];
+  }
+  
+  [self.buttonToggleRecord setTitle: (self.audioRecorder.isRecording ? @"Pause" : @"Record")
+                           forState: UIControlStateNormal];
+}
+@end
+#endif
